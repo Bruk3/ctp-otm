25296fd723f032d9f557fa83efd9063326a9dded
Bruk Zewdie <bruk.zewdie@joinhoney.com>
2020-07-27 09:59:08 -0400

    Updates

---
+++ precache-manifest.5f80d73ab90cb07f06ec8ca2cac60c3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d198f4ed670145dc4c8c8c3004b4a9ff",
+    "url": "/ctp-otm/index.html"
+  },
+  {
+    "revision": "42b655cdcac5f7efd3e4",
+    "url": "/ctp-otm/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1740b0cb781048a7bdcc",
+    "url": "/ctp-otm/static/js/2.536f6268.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ctp-otm/static/js/2.536f6268.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42b655cdcac5f7efd3e4",
+    "url": "/ctp-otm/static/js/main.e119fede.chunk.js"
+  },
+  {
+    "revision": "7d3cea1bdc7350f1496f",
+    "url": "/ctp-otm/static/js/runtime-main.d4ef3ec0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ctp-otm/static/media/logo.5d5d9eef.svg"
+  }
+]);
